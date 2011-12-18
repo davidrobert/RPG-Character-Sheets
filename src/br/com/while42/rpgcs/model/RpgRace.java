c9689ed2ce60f2c33f1ecf4ea3ed346ab66cd634
David Robert <davidrobert@gmail.com>
2011-12-18 18:26:02 -0200

    Add race

--- src/br/com/while42/rpgcs/model/RpgRace.java
+++ src/br/com/while42/rpgcs/model/RpgRace.java
@@ -1,5 +1,42 @@
 package br.com.while42.rpgcs.model;
 
-public enum RpgRace {
+import android.content.Context;
+import br.com.while42.rpgcs.R;
 
+public enum RpgRace {
+	HUMAN{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.human);
+		}
+	},
+    ELF{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.elf);
+		}
+	},
+    DWARF{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.dwarf);
+		}
+	},
+    HALFLING{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.halfling);
+		}
+	}, 
+    HALF_ELF{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.half_elf);
+		}
+	},
+    HALF_ORC{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.half_orc);
+		}
+	}, 
+    GNOME{
+		public String toString(Context ctx) {
+			return ctx.getString(R.string.gnome);
+		}
+	}
 }
