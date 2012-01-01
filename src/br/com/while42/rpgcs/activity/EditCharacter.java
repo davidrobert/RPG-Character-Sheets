798717fb6286e009ca8c887856c5cf24e7202361
David Robert <davidrobert@gmail.com>
2012-01-01 19:55:40 -0200

    removing unnecessary files

--- src/br/com/while42/rpgcs/activity/EditCharacter.java
+++
@@ -1,9 +0,0 @@
-package br.com.while42.rpgcs.activity;
-
-import br.com.while42.rpgcs.model.RpgCharacter;
-
-import android.app.Activity;
-
-public class EditCharacter extends Activity {
-	private RpgCharacter rpgCharacter;
-}
