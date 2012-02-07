79a905c60d1d663df9906387db479ab290d8c1ec
David Robert <davidrobert@gmail.com>
2012-02-07 14:45:08 -0200

    Add tests to getMaxValue and getMinValue

--- RPG-Character-Sheets-Test/src/br/com/while42/rpgcs/model/HitDiceTest.java
+++ RPG-Character-Sheets-Test/src/br/com/while42/rpgcs/model/HitDiceTest.java
@@ -22,5 +22,33 @@ public class HitDiceTest extends AndroidTestCase {
 		Assert.assertEquals("1d6+1", new HitDice(1, HitDiceType.d6, +1).toString());
 		Assert.assertEquals("3d8", new HitDice(3, HitDiceType.d8, 0).toString());
 	}
+	
+	public void testgetMaxValue() throws Throwable {		
+		Assert.assertEquals(4, new HitDice(HitDiceType.d4).getMaxValue());
+		Assert.assertEquals(6, new HitDice(HitDiceType.d6).getMaxValue());
+		Assert.assertEquals(8, new HitDice(HitDiceType.d8).getMaxValue());
+		
+		Assert.assertEquals(2 * 4, new HitDice(2, HitDiceType.d4).getMaxValue());
+		Assert.assertEquals(1 * 6, new HitDice(1, HitDiceType.d6).getMaxValue());
+		Assert.assertEquals(3 * 8, new HitDice(3, HitDiceType.d8).getMaxValue());
+		
+		Assert.assertEquals(2 * 8 - 1, new HitDice(2, HitDiceType.d4, -1).getMaxValue());
+		Assert.assertEquals(1 * 6 + 1, new HitDice(1, HitDiceType.d6, +1).getMaxValue());
+		Assert.assertEquals(3 * 8 + 0, new HitDice(3, HitDiceType.d8, 0).getMaxValue());
+	}
 
+	public void testgetMinValue() throws Throwable {		
+		Assert.assertEquals(1, new HitDice(HitDiceType.d4).getMinValue());
+		Assert.assertEquals(1, new HitDice(HitDiceType.d6).getMinValue());
+		Assert.assertEquals(1, new HitDice(HitDiceType.d8).getMinValue());
+		
+		Assert.assertEquals(2 * 1, new HitDice(2, HitDiceType.d4).getMinValue());
+		Assert.assertEquals(1 * 1, new HitDice(1, HitDiceType.d6).getMinValue());
+		Assert.assertEquals(3 * 1, new HitDice(3, HitDiceType.d8).getMinValue());
+		
+		Assert.assertEquals(2 * 1 - 1, new HitDice(2, HitDiceType.d4, -1).getMinValue());
+		Assert.assertEquals(1 * 1 + 1, new HitDice(1, HitDiceType.d6, +1).getMinValue());
+		Assert.assertEquals(3 * 1 + 0, new HitDice(3, HitDiceType.d8, 0).getMinValue());
+	}	
+	
 }
