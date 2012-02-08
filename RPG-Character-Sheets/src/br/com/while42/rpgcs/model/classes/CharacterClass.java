c296c6fbfad43a14d458fdc99cc82d42699ce8be
David Robert <davidrobert@gmail.com>
2012-02-08 14:12:47 -0200

    Removed modifiers in methods

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/classes/CharacterClass.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/classes/CharacterClass.java
@@ -5,10 +5,10 @@ import br.com.while42.rpgcs.model.character.attributes.TypeRpgClass;
 
 public interface CharacterClass {
 	
-	public HitDiceType getHitDice();
-	public TypeRpgClass getClassType();
-	public int getBaseAttackBonus();
-	public int getFortSave();
-	public int getRefSave();
-	public int getWillSave();
+	HitDiceType getHitDice();
+	TypeRpgClass getClassType();
+	int getBaseAttackBonus();
+	int getFortSave();
+	int getRefSave();
+	int getWillSave();
 }
