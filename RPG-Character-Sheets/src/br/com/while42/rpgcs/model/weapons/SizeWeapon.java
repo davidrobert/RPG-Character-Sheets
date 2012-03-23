642ae92cab81b6592914d433a6a8b2e98cf4f406
David Robert <davidrobert@gmail.com>
2012-03-23 17:33:07 -0300

    Add size weapon

---
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/weapons/SizeWeapon.java
@@ -0,0 +1,19 @@
+package br.com.while42.rpgcs.model.weapons;
+
+import br.com.while42.rpgcs.R;
+
+public enum SizeWeapon {
+	TINY(R.string.size_tiny),
+	MEDIUM(R.string.size_medium),
+	LARGE(R.string.size_large);
+	
+	private int nameCode;
+
+	private SizeWeapon(int nameCode) {
+		this.nameCode = nameCode;
+	}
+
+	public int getNameCode() {
+		return nameCode;
+	}
+}
--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/weapons/Weapon.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/weapons/Weapon.java
@@ -7,8 +7,8 @@ import br.com.while42.rpgcs.model.HitDice;
 public interface Weapon {
 	
 	public int getCost();
-	public HitDice getDamageSmall();
-	public HitDice getDamageMedium();
+	public HitDice getDamage();
+	public HitDice getDamage(SizeWeapon size);
 	public int getCritical();
 	public int getRangeIncrement();
 	public float getWeight();
