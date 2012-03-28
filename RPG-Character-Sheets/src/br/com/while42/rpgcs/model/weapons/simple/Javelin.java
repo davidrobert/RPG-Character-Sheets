67caae79b328a0b8d01d59089a7edff64889b4ba
David Robert <davidrobert@gmail.com>
2012-03-28 09:25:07 -0300

    New Weapon (Javelin)

---
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/weapons/simple/Javelin.java
@@ -0,0 +1,74 @@
+package br.com.while42.rpgcs.model.weapons.simple;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import br.com.while42.rpgcs.R;
+import br.com.while42.rpgcs.model.HitDice;
+import br.com.while42.rpgcs.model.HitDiceType;
+import br.com.while42.rpgcs.model.weapons.AbstractSimpleWeapon;
+import br.com.while42.rpgcs.model.weapons.SizeWeapon;
+import br.com.while42.rpgcs.model.weapons.TypeWeapon;
+
+public class Javelin extends AbstractSimpleWeapon {
+
+	private static final int nameCode = R.string.weapon_simple_javelin;
+	private static final int cost = 1;
+	private static final Map<SizeWeapon, HitDice> damage = new HashMap<SizeWeapon, HitDice>();	
+	private static final int critical = 2;
+	private static final int rangeIncrement = 9;
+	private static final double weight = 1;
+	private static final Set<TypeWeapon> types = new HashSet<TypeWeapon>();
+	
+	static {
+		damage.put(SizeWeapon.SMALL, new HitDice(HitDiceType.d4));
+		damage.put(SizeWeapon.MEDIUM, new HitDice(HitDiceType.d6));
+		
+		types.add(TypeWeapon.PIERCING);
+	}
+	
+	
+	@Override
+	public int getNamecode() {
+		return nameCode;
+	}
+	
+	@Override
+	public int getCost() {
+		return cost;
+	}
+
+	@Override
+	public HitDice getDamage() {
+		return getDamage(SizeWeapon.MEDIUM);
+	}
+
+	@Override
+	public HitDice getDamage(SizeWeapon size) {
+		return damage.get(size);
+	}
+
+	@Override
+	public int getCritical() {
+		return critical;
+	}
+
+	@Override
+	public int getRangeIncrement() {
+		return rangeIncrement;
+	}
+
+	@Override
+	public double getWeight() {
+		return weight;
+	}
+
+	@Override
+	public Set<TypeWeapon> getType() {
+		return Collections.unmodifiableSet(types);
+	}
+
+}
