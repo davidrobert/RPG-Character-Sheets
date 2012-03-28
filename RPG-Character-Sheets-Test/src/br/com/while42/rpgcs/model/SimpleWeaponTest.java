ad296dc2ee8bd2a639e14b96b701547fc2c9528b
David Robert <davidrobert@gmail.com>
2012-03-28 14:10:38 -0300

    Add method test of code name

--- RPG-Character-Sheets-Test/src/br/com/while42/rpgcs/model/SimpleWeaponTest.java
+++ RPG-Character-Sheets-Test/src/br/com/while42/rpgcs/model/SimpleWeaponTest.java
@@ -2,6 +2,7 @@ package br.com.while42.rpgcs.model;
 
 import junit.framework.Assert;
 import android.test.AndroidTestCase;
+import br.com.while42.rpgcs.R;
 import br.com.while42.rpgcs.model.weapons.simple.Club;
 import br.com.while42.rpgcs.model.weapons.simple.CrossbowHeavy;
 import br.com.while42.rpgcs.model.weapons.simple.CrossbowLight;
@@ -26,32 +27,58 @@ public class SimpleWeaponTest extends AndroidTestCase {
 		
 	/* 
 	public enum SimpleWeapon implements BasicWeapon {
-		GAUNTLET       (R.string.weapon_simple_gauntlet       , 2, new HitDice(HitDiceType.d2), new HitDice(HitDiceType.d3 ), 2,  0,  0.5, TypeWeapon.BLUDGEONING),
-		UNARMED_STRIKE (R.string.weapon_simple_unarmed_strike , 0, new HitDice(HitDiceType.d2), new HitDice(HitDiceType.d3 ), 2,  0,    0, TypeWeapon.BLUDGEONING),
+		GAUNTLET       (, 2, new HitDice(HitDiceType.d2), new HitDice(HitDiceType.d3 ), 2,  0,  0.5, TypeWeapon.BLUDGEONING),
+		UNARMED_STRIKE (, 0, new HitDice(HitDiceType.d2), new HitDice(HitDiceType.d3 ), 2,  0,    0, TypeWeapon.BLUDGEONING),
 		
-		DAGGER         (R.string.weapon_simple_dagger         , 2, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2,  3,  0.5, TypeWeapon.PIERCING, TypeWeapon.SLASHING),
-		DAGGER_PUNCHING(R.string.weapon_simple_dagger_punching, 2, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 3,  0,  0.5, TypeWeapon.PIERCING),
-		GAUNTLET_SPIKED(R.string.weapon_simple_gauntlet_spiked, 5, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2,  0,  0.5, TypeWeapon.PIERCING),
-		MACE_LIGHT     (R.string.weapon_simple_mace_light     , 5, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  0,    2, TypeWeapon.BLUDGEONING),
-		SICKLE         (R.string.weapon_simple_sickle         , 6, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  0,    1, TypeWeapon.SLASHING),
+		DAGGER         (, 2, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2,  3,  0.5, TypeWeapon.PIERCING, TypeWeapon.SLASHING),
+		DAGGER_PUNCHING(, 2, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 3,  0,  0.5, TypeWeapon.PIERCING),
+		GAUNTLET_SPIKED(, 5, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2,  0,  0.5, TypeWeapon.PIERCING),
+		MACE_LIGHT     (, 5, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  0,    2, TypeWeapon.BLUDGEONING),
+		SICKLE         (, 6, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  0,    1, TypeWeapon.SLASHING),
 		
-		CLUB           (R.string.weapon_simple_club           , 0, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  3,  1.5, TypeWeapon.BLUDGEONING),
-		MACE_HEAVY     (R.string.weapon_simple_mace_heavy     ,12, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 2,  0,    4, TypeWeapon.BLUDGEONING),
-		MORNINGSTAR    (R.string.weapon_simple_morningstar    , 8, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 2,  0,    3, TypeWeapon.BLUDGEONING, TypeWeapon.PIERCING),
-		SHORTSPEAR     (R.string.weapon_simple_shortspear     , 1, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  6,  1.5, TypeWeapon.PIERCING),
+		CLUB           (, 0, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  3,  1.5, TypeWeapon.BLUDGEONING),
+		MACE_HEAVY     (,12, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 2,  0,    4, TypeWeapon.BLUDGEONING),
+		MORNINGSTAR    (, 8, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 2,  0,    3, TypeWeapon.BLUDGEONING, TypeWeapon.PIERCING),
+		SHORTSPEAR     (, 1, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  6,  1.5, TypeWeapon.PIERCING),
 		
-		LONGSPEAR      (R.string.weapon_simple_longspear      , 5, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 3,  0,  4.5, TypeWeapon.PIERCING),
-		QUARTESTAFF    (R.string.weapon_simple_quartestaff    , 0, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  0,    2, TypeWeapon.BLUDGEONING),
-		SPEAR          (R.string.weapon_simple_spear          , 2, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 3,  6,    3, TypeWeapon.PIERCING),
+		LONGSPEAR      (, 5, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 3,  0,  4.5, TypeWeapon.PIERCING),
+		QUARTESTAFF    (, 0, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  0,    2, TypeWeapon.BLUDGEONING),
+		SPEAR          (, 2, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 3,  6,    3, TypeWeapon.PIERCING),
 		
-		CROSSBOW_HEAVY (R.string.weapon_simple_crossbow_heavy ,50, new HitDice(HitDiceType.d8), new HitDice(HitDiceType.d10), 2, 36,    4, TypeWeapon.PIERCING),
-		CROSSBOW_LIGHT (R.string.weapon_simple_crossbow_light ,35, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 2, 24,    2, TypeWeapon.PIERCING),
-		DART           (R.string.weapon_simple_dart           , 5, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2,  6, 0.25, TypeWeapon.PIERCING),
-		JAVELIN        (R.string.weapon_simple_javelin        , 1, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  9,    1, TypeWeapon.PIERCING),
-		SLING          (R.string.weapon_simple_sling          , 0, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2, 15,    0, TypeWeapon.BLUDGEONING);
+		CROSSBOW_HEAVY (,50, new HitDice(HitDiceType.d8), new HitDice(HitDiceType.d10), 2, 36,    4, TypeWeapon.PIERCING),
+		CROSSBOW_LIGHT (,35, new HitDice(HitDiceType.d6), new HitDice(HitDiceType.d8 ), 2, 24,    2, TypeWeapon.PIERCING),
+		DART           (, 5, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2,  6, 0.25, TypeWeapon.PIERCING),
+		JAVELIN        (, 1, new HitDice(HitDiceType.d4), new HitDice(HitDiceType.d6 ), 2,  9,    1, TypeWeapon.PIERCING),
+		SLING          (, 0, new HitDice(HitDiceType.d3), new HitDice(HitDiceType.d4 ), 2, 15,    0, TypeWeapon.BLUDGEONING);
 	}
 
 	*/
+	
+	public void testCodeName() throws Throwable {		
+		Assert.assertEquals(R.string.weapon_simple_gauntlet,        new Gauntlet().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_unarmed_strike,  new UnarmedStrike().getCodeName());
+		
+		Assert.assertEquals(R.string.weapon_simple_dagger,          new Dagger().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_dagger_punching, new DaggerPunching().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_gauntlet_spiked, new GauntletSpiked().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_mace_light,      new MaceLight().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_sickle,          new Sickle().getCodeName());
+		
+		Assert.assertEquals(R.string.weapon_simple_club,            new Club().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_mace_heavy,      new MaceHeavy().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_morningstar,     new Morningstar().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_shortspear,      new Shortspear().getCodeName());
+		
+		Assert.assertEquals(R.string.weapon_simple_longspear,       new Longspear().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_quartestaff,     new Quartestaff().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_spear,           new Spear().getCodeName());
+		
+		Assert.assertEquals(R.string.weapon_simple_crossbow_heavy,  new CrossbowHeavy().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_crossbow_light,  new CrossbowLight().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_dart,            new Dart().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_javelin,         new Javelin().getCodeName());
+		Assert.assertEquals(R.string.weapon_simple_sling,           new Sling().getCodeName());
+	}
 	
 	public void testCost() throws Throwable {		
 		Assert.assertEquals( 2, new Gauntlet().getCost());
