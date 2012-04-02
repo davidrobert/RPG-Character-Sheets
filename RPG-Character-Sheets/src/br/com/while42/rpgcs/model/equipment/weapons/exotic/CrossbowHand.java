6c534594072cff9ba99089513451b4329c6149e6
David Robert <davidrobert@gmail.com>
2012-04-02 09:16:02 -0300

    Small correction: CrossbowHand

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/equipment/weapons/exotic/CrossbowHand.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/equipment/weapons/exotic/CrossbowHand.java
@@ -13,13 +13,13 @@ public class CrossbowHand extends AbstractExoticWeapon {
 
 	static {
 		base = new BasicWeapon(R.string.weapon_exotic_crossbow_hand);
-		base.setCost(2);
+		base.setCost(100);
 		base.setCritical(2);
-		base.setRangeIncrement(0);
+		base.setRangeIncrement(9);
 		base.setWeight(1);
 		
-		base.addDamage(SizeWeapon.SMALL, new HitDice(HitDiceType.d4));
-		base.addDamage(SizeWeapon.MEDIUM, new HitDice(HitDiceType.d6));
+		base.addDamage(SizeWeapon.SMALL, new HitDice(HitDiceType.d3));
+		base.addDamage(SizeWeapon.MEDIUM, new HitDice(HitDiceType.d4));
 		
 		base.addType(TypeWeapon.SLASHING);
 	}
