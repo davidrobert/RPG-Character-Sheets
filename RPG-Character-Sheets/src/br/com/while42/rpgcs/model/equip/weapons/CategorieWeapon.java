edb62d00231601e814577675e6ff2ce96d4f9863
David Robert <davidrobert@gmail.com>
2012-04-02 15:15:10 -0300

    Diferents categories

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/equip/weapons/CategorieWeapon.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/equip/weapons/CategorieWeapon.java
@@ -5,9 +5,7 @@ import br.com.while42.rpgcs.R;
 public enum CategorieWeapon {
 	SIMPLE(R.string.weapon_categorie_simple),
 	MARTIAL(R.string.weapon_categorie_martial),
-	EXOTIC(R.string.weapon_categorie_exotic),
-	MELEE(R.string.weapon_categorie_melle),
-	RANGED(R.string.weapon_categorie_ranged);
+	EXOTIC(R.string.weapon_categorie_exotic);
 	
 	private int nameCode;
 
