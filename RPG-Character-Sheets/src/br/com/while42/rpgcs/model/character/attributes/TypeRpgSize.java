46c782651fb8ece16d423f0580f8522de9271ed7
David Robert <davidrobert@gmail.com>
2012-04-05 09:25:34 -0300

    Size modifier

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/character/attributes/TypeRpgSize.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/character/attributes/TypeRpgSize.java
@@ -4,23 +4,30 @@ import br.com.while42.rpgcs.R;
 import br.com.while42.rpgcs.model.TypeCode;
 
 public enum TypeRpgSize implements TypeCode {
-	FINE(R.string.size_fine), 
-	DIMINUTTIVE(R.string.size_diminutive), 
-	TINY(R.string.size_tiny), 
-	SMALL(R.string.size_small), 
-	MEDIUM(R.string.size_medium), 
-	LARGE(R.string.size_large), 
-	HUGE(R.string.size_huge), 
-	GARGANTUAN(R.string.size_gargantuan), 
-	COLOSSAL(R.string.size_gargantuan);
+	FINE       (+8, R.string.size_fine), 
+	DIMINUTTIVE(+4, R.string.size_diminutive), 
+	TINY       (+2, R.string.size_tiny), 
+	SMALL      (+1, R.string.size_small), 
+	MEDIUM     (+0, R.string.size_medium), 
+	LARGE      (-1, R.string.size_large), 
+	HUGE       (-2, R.string.size_huge), 
+	GARGANTUAN (-4, R.string.size_gargantuan), 
+	COLOSSAL   (-8, R.string.size_gargantuan);
 	
+	private int sizeModifier;
 	private int nameCode;
 
-	private TypeRpgSize(int nameCode) {
+	private TypeRpgSize(int sizeModifier, int nameCode) {
+		this.sizeModifier = sizeModifier;
 		this.nameCode = nameCode;
 	}
 
+	@Override
 	public int getCodeName() {
 		return nameCode;
+	}
+	
+	public int getSizeModifier() {
+		return sizeModifier;
 	}
 }
