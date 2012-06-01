51eeed3deedd067a836050a569b2a21a52ccf16f
David Robert <davidrobert@gmail.com>
2012-05-31 23:20:49 -0300

    Persist RpgCharacterTable

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/persist/table/RpgCharacterTable.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/persist/table/RpgCharacterTable.java
@@ -9,9 +9,36 @@ public final class RpgCharacterTable {
 
 	public static class RpgCharacterColumns implements BaseColumns {
 		public static final String NAME = "name";
+		public static final String RACE = "race";	
+		public static final String ALIGNMENT = "alignment";
+		public static final String RELIGION = "religion";
+		
+		public static final String SIZE = "size";
+		public static final String AGE = "age";	
+		public static final String GENDER = "gender";
+		public static final String HEIGHT = "height";
+		public static final String WEIGHT = "weight";
+		public static final String EYE = "eye";
+		public static final String HAIR = "hair";
+		public static final String SKIN = "skin";
 
 		public static String[] get() {
-			return new String[] { BaseColumns._ID, RpgCharacterColumns.NAME };
+			return new String[] { 
+					BaseColumns._ID, 
+					RpgCharacterColumns.NAME,
+					RpgCharacterColumns.RACE,
+					RpgCharacterColumns.ALIGNMENT,
+					RpgCharacterColumns.RELIGION,
+					
+					RpgCharacterColumns.SIZE,					
+					RpgCharacterColumns.AGE,
+					RpgCharacterColumns.GENDER,
+					RpgCharacterColumns.HEIGHT,
+					RpgCharacterColumns.WEIGHT,
+					RpgCharacterColumns.EYE,
+					RpgCharacterColumns.HAIR,
+					RpgCharacterColumns.SKIN
+			};
 		}
 	}
 
@@ -19,7 +46,19 @@ public final class RpgCharacterTable {
 		StringBuilder sb = new StringBuilder();
 		sb.append("CREATE TABLE " + RpgCharacterTable.NAME + " (");
 		sb.append(BaseColumns._ID + " INTEGER PRIMARY KEY, ");
-		sb.append(RpgCharacterColumns.NAME + " TEXT UNIQUE NOT NULL");
+		sb.append(RpgCharacterColumns.NAME + " TEXT");
+		sb.append(RpgCharacterColumns.RACE + " TEXT");
+		sb.append(RpgCharacterColumns.ALIGNMENT + " TEXT");
+		sb.append(RpgCharacterColumns.RELIGION + " TEXT");
+		
+		sb.append(RpgCharacterColumns.SIZE + " TEXT");
+		sb.append(RpgCharacterColumns.AGE + " TEXT");
+		sb.append(RpgCharacterColumns.GENDER + " TEXT");
+		sb.append(RpgCharacterColumns.HEIGHT + " TEXT");
+		sb.append(RpgCharacterColumns.WEIGHT + " TEXT");
+		sb.append(RpgCharacterColumns.EYE + " TEXT");
+		sb.append(RpgCharacterColumns.HAIR + " TEXT");
+		sb.append(RpgCharacterColumns.SKIN + " TEXT");
 		sb.append(");");
 
 		db.execSQL(sb.toString());
