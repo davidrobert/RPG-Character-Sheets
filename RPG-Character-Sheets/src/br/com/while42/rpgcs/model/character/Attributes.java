a83c405d5c91d4026852dec2b9ec8b4ca1d6c098
David Robert <davidrobert@gmail.com>
2012-06-10 16:41:00 -0300

    Add default values

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/character/Attributes.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/character/Attributes.java
@@ -28,6 +28,21 @@ public class Attributes implements Serializable {
 	private TypeHairColor hair;
 	private TypeSkinColor skin;
 
+	public Attributes() {		
+		// Deafult values
+		race = TypeRpgRace.HUMAN;
+		alignment = TypeRpgAlignment.TRUE_NEUTRAL;
+		religion = TypeRpgReligion.NONE;
+		size = TypeRpgSize.MEDIUM;
+		age = 21;
+		gender = TypeGender.MEN;
+		height = 175;
+		weight = 80;
+		eye = TypeEyeColor.Brown;
+		hair = TypeHairColor.BROWN;
+		skin = TypeSkinColor.BROWN;
+	}
+	
 	public TypeRpgRace getRace() {
 		return race;
 	}
