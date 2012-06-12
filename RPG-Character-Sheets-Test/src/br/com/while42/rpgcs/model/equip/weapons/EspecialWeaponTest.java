8510487b31ddfdfc803ef31c524f24f972dce3a5
David Robert <davidrobert@gmail.com>
2012-06-12 13:30:21 -0300

    Completion of tests (BuilderEspecialWeapon)

--- RPG-Character-Sheets-Test/src/br/com/while42/rpgcs/model/equip/weapons/EspecialWeaponTest.java
+++ RPG-Character-Sheets-Test/src/br/com/while42/rpgcs/model/equip/weapons/EspecialWeaponTest.java
@@ -1,6 +1,13 @@
 package br.com.while42.rpgcs.model.equip.weapons;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import android.test.AndroidTestCase;
+import br.com.while42.rpgcs.model.HitDice;
+import br.com.while42.rpgcs.model.HitDiceType;
 import br.com.while42.rpgcs.model.equip.weapons.especial.BuilderEspecialWeapon;
 import br.com.while42.rpgcs.model.equip.weapons.especial.EspecialWeapon;
 
@@ -15,12 +22,28 @@ public class EspecialWeaponTest extends AndroidTestCase {
 		                                 .setWeight(0.25)
 		                                 .setSize(SizeWeapon.MEDIUM)
 		                                 .setType(TypeWeapon.PIERCING)
+		                                 .setDamage(new HitDice(2, HitDiceType.d6, +2))
 		                                 .setCategorieEncumbrance(CategorieEncumbranceWeapon.TWO_HANDED)
 		                                 .setCategorieUsefulness(CategorieUsefulnessWeapon.MELEE)
 		                                 .toEspecialWeapon();
 		
+		Set<TypeWeapon> types = new HashSet<TypeWeapon>();
+		types.add(TypeWeapon.PIERCING);
+		
+		List<HitDice> damages = new ArrayList<HitDice>();
+		damages.add(new HitDice(2, HitDiceType.d6, +2));
+		
 		assertEquals("Arma Especial", especial.getName());
-		       
+		assertEquals(10000, especial.getCost().intValue());
+		assertEquals(2, especial.getCritical());
+		assertEquals(0, especial.getRangeIncrement());
+		assertEquals(0.25, especial.getWeight());
+		assertEquals(SizeWeapon.MEDIUM, especial.getSize());
+		assertEquals(types, especial.getType());
+		assertEquals(damages, especial.getDamage());
+		assertEquals(CategorieEncumbranceWeapon.TWO_HANDED, especial.getCategorieEncumbrance());
+		assertEquals(CategorieUsefulnessWeapon.MELEE, especial.getCategorieUsefulness());
+		    
 	}
 	
 	
