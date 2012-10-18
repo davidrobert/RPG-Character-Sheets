ad64f3bc68f63546cd51881d44e411da184e6f54
SuelenGC <suelengcarvalho@gmail.com>
2012-10-18 03:52:01 -0300

    Created new layout base to Play screen

---
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/activity/PlayRpgCharacterNew.java
@@ -0,0 +1,16 @@
+package br.com.while42.rpgcs.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import br.com.while42.rpgcs.R;
+
+public class PlayRpgCharacterNew extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_play_character_smartphone);
+
+	}
+}
