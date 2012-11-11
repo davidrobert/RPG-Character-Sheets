21c7b1381c5ae477074c372eb25b6c7f07a4bb08
David Robert <davidrobert@gmail.com>
2012-11-11 10:36:12 -0200

    Edit Layout

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/activity/fragment/EditCharacteristics.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/activity/fragment/EditCharacteristics.java
@@ -17,14 +17,28 @@ import br.com.while42.rpgcs.model.TypeCode;
 import br.com.while42.rpgcs.model.character.Attributes;
 import br.com.while42.rpgcs.model.character.RpgCharacter;
 import br.com.while42.rpgcs.model.character.attributes.TypeGender;
+import br.com.while42.rpgcs.model.character.attributes.TypeHairColor;
 import br.com.while42.rpgcs.model.character.attributes.TypeRpgAlignment;
 import br.com.while42.rpgcs.model.character.attributes.TypeRpgRace;
 import br.com.while42.rpgcs.model.character.attributes.TypeRpgReligion;
+import br.com.while42.rpgcs.model.character.attributes.TypeSkinColor;
 import br.com.while42.rpgcs.model.character.attributes.TypeVision;
 
 public class EditCharacteristics extends Fragment {
 
 	private RpgCharacter rpgCharacter;
+
+	private EditText nameEditText;
+	private Spinner raceSpinner;
+	private Spinner alignmentSpinner;
+	private Spinner genderSpinner;
+	private Spinner religionSpinner;
+	private Spinner visionSpinner;
+	private EditText ageEditText;
+	private EditText heightEditText;
+	private EditText weightEditText;
+	private Spinner hairSpinner;
+	private Spinner skinSpinner;
 
 	public EditCharacteristics() {
 	}
@@ -38,33 +52,49 @@ public class EditCharacteristics extends Fragment {
 
 		View layout = inflater.inflate(R.layout.fragment_edit_characteristics, container, false);
 
-		EditText etName = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_name);
-		Spinner sRace = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_race);
-		Spinner sAlignment = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_alignment);
-		Spinner sGender = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_gender);
-		Spinner sReligion = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_religion);
-		Spinner sVision = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_vision);
-		EditText etAge = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_age);
-		EditText etHeight = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_height);
-		EditText etWeight = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_weight);
+		nameEditText = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_name);
+		raceSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_race);
+		alignmentSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_alignment);
+		genderSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_gender);
+		religionSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_religion);
+		visionSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_vision);
+		ageEditText = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_age);
+		heightEditText = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_height);
+		weightEditText = (EditText) layout.findViewById(R.id_frag_edit_characteristics.edittext_weight);
+		hairSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_hair);
+		skinSpinner = (Spinner) layout.findViewById(R.id_frag_edit_characteristics.spinner_skin);
 
-		setOptionsSpinner(sRace, TypeRpgRace.values());
-		setOptionsSpinner(sAlignment, TypeRpgAlignment.values());
-		setOptionsSpinner(sGender, TypeGender.values());
-		setOptionsSpinner(sReligion, TypeRpgReligion.values());
-		setOptionsSpinner(sVision, TypeVision.values());
+		setOptionsSpinner(raceSpinner, TypeRpgRace.values());
+		setOptionsSpinner(alignmentSpinner, TypeRpgAlignment.values());
+		setOptionsSpinner(genderSpinner, TypeGender.values());
+		setOptionsSpinner(religionSpinner, TypeRpgReligion.values());
+		setOptionsSpinner(visionSpinner, TypeVision.values());
+		setOptionsSpinner(hairSpinner, TypeHairColor.values());
+		setOptionsSpinner(skinSpinner, TypeSkinColor.values());
 
-		if (rpgCharacter == null)
-			return layout;
+		loadCharacteristics(rpgCharacter);
+
+		return layout;
+	}
+
+	private void loadCharacteristics(RpgCharacter rpgCharacter) {
 		
+		if (rpgCharacter == null)
+			return;
+
 		Attributes attributes = rpgCharacter.getAttributes();
 
-		etName.setText(rpgCharacter.getName());
-		etAge.setText(attributes.getAge().toString());
-		etHeight.setText(attributes.getHeight().toString());
-		etWeight.setText(attributes.getWeight().toString());
-		
-		return layout;
+		nameEditText.setText(rpgCharacter.getName());
+		// TODO: Race
+		// TODO: Aligment
+		// TODO: Gender
+		// TODO: Religion
+		// TODO: Vision
+		ageEditText.setText(attributes.getAge().toString());
+		heightEditText.setText(attributes.getHeight().toString());
+		weightEditText.setText(attributes.getWeight().toString());
+		// TODO: Hair
+		// TODO: Skin
 	}
 
 	private void setOptionsSpinner(Spinner spinner, TypeCode[] types) {
