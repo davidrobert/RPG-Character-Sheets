3259d9690b15fae278aa967daf5d5f0c58a46305
David Robert <davidrobert@gmail.com>
2012-11-13 16:52:58 -0200

    Add constructor with Characteristics

--- RPG-Character-Sheets/src/br/com/while42/rpgcs/model/character/BuilderCharacteristics.java
+++ RPG-Character-Sheets/src/br/com/while42/rpgcs/model/character/BuilderCharacteristics.java
@@ -13,7 +13,7 @@ import br.com.while42.rpgcs.model.character.attributes.TypeVision;
 public class BuilderCharacteristics {
 
 	private String name;
-	
+
 	private TypeRpgRace race;
 	private TypeRpgAlignment alignment;
 	private TypeRpgReligion religion;
@@ -27,126 +27,147 @@ public class BuilderCharacteristics {
 	private TypeHairColor hair;
 	private TypeSkinColor skin;
 	private TypeVision vision;
-	
+
+	private Characteristics characteristics;
+
+	public BuilderCharacteristics() {
+		this.characteristics = new Characteristics();
+	}
+
+	public BuilderCharacteristics(Characteristics characteristics) {
+		this.characteristics = new Characteristics(characteristics);
+	}
+
 	public BuilderCharacteristicsWithName setName(String name) {
 		return new BuilderCharacteristicsWithName(name);
 	}
-	
+
 	public class BuilderCharacteristicsWithName {
 		private BuilderCharacteristicsWithName(String n) {
 			name = n;
 		}
+
 		public BuilderCharacteristicsWithRace setRace(TypeRpgRace race) {
 			return new BuilderCharacteristicsWithRace(race);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithRace {
 		private BuilderCharacteristicsWithRace(TypeRpgRace r) {
 			race = r;
 		}
+
 		public BuilderCharacteristicsWithAlignment setAlignment(TypeRpgAlignment alignment) {
 			return new BuilderCharacteristicsWithAlignment(alignment);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithAlignment {
 		private BuilderCharacteristicsWithAlignment(TypeRpgAlignment a) {
 			alignment = a;
 		}
+
 		public BuilderCharacteristicsWithReligion setReligion(TypeRpgReligion religion) {
 			return new BuilderCharacteristicsWithReligion(religion);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithReligion {
 		private BuilderCharacteristicsWithReligion(TypeRpgReligion r) {
 			religion = r;
 		}
+
 		public BuilderCharacteristicsWithSize setSize(TypeRpgSize size) {
 			return new BuilderCharacteristicsWithSize(size);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithSize {
 		private BuilderCharacteristicsWithSize(TypeRpgSize s) {
 			size = s;
 		}
+
 		public BuilderCharacteristicsWithAge setAge(Integer age) {
 			return new BuilderCharacteristicsWithAge(age);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithAge {
 		private BuilderCharacteristicsWithAge(Integer a) {
 			age = a;
 		}
+
 		public BuilderCharacteristicsWithGender setGender(TypeGender gender) {
 			return new BuilderCharacteristicsWithGender(gender);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithGender {
 		private BuilderCharacteristicsWithGender(TypeGender g) {
 			gender = g;
 		}
+
 		public BuilderCharacteristicsWithHeight setHeight(Integer height) {
 			return new BuilderCharacteristicsWithHeight(height);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithHeight {
 		private BuilderCharacteristicsWithHeight(Integer h) {
 			height = h;
 		}
+
 		public BuilderCharacteristicsWithWeight setWeight(Integer weight) {
 			return new BuilderCharacteristicsWithWeight(weight);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithWeight {
 		private BuilderCharacteristicsWithWeight(Integer w) {
 			weight = w;
 		}
+
 		public BuilderCharacteristicsWithEye setEye(TypeEyeColor color) {
 			return new BuilderCharacteristicsWithEye(color);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithEye {
 		private BuilderCharacteristicsWithEye(TypeEyeColor color) {
 			eye = color;
 		}
+
 		public BuilderCharacteristicsWithHair setHair(TypeHairColor color) {
 			return new BuilderCharacteristicsWithHair(color);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithHair {
 		private BuilderCharacteristicsWithHair(TypeHairColor color) {
 			hair = color;
 		}
+
 		public BuilderCharacteristicsWithSkin setSkin(TypeSkinColor color) {
 			return new BuilderCharacteristicsWithSkin(color);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithSkin {
 		private BuilderCharacteristicsWithSkin(TypeSkinColor color) {
 			skin = color;
 		}
+
 		public BuilderCharacteristicsWithVision setVision(TypeVision vision) {
 			return new BuilderCharacteristicsWithVision(vision);
 		}
 	}
-	
+
 	public class BuilderCharacteristicsWithVision {
 		private BuilderCharacteristicsWithVision(TypeVision v) {
 			vision = v;
 		}
 
-		public Characteristics toAttributes() {
-			Characteristics characteristics = new Characteristics();
+		public Characteristics toCharacteristics() {
 
 			characteristics.setName(name);
 			characteristics.setRace(race);
@@ -161,9 +182,9 @@ public class BuilderCharacteristics {
 			characteristics.setHair(hair);
 			characteristics.setSkin(skin);
 			characteristics.setVision(vision);
-			
+
 			return characteristics;
 		}
-		
+
 	}
 }
